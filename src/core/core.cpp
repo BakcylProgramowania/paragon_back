e03d1789f6467e0633c935cf68c32720f8adc506
wladek <135755358+wladek2136@users.noreply.github.com>
2023-12-08 23:00:20 +0100

    odebranie i przekonwertowanie jsona

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -1,2 +1,53 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include "json.hpp"
+#include <string>
+
+using json = nlohmann::json;
+
+struct Product {
+    std::string name;
+    int amount;
+    double price;
+    int whoBoughtID;
+};
+
+struct User {
+    int id;
+    std::string name;
+    std::string phone;
+};
+int main() {
+    std::vector<Product> products;
+    std::vector<User> users;
+  
+  std::string jsonData = "
+  {"receipt":[{"products":[{"product":"sth","amount":sth,"price":sth,"whoBought",{"personiD":sth,
+"personID":sth}]},
+  {"product":"sth","amount":sth,"price":sth,"whoBought":2}]}],
+"users":[{id":1,"name":"sth","phone":"sth"},
+  {"id":2,"name":"sth","phone":"sth"}]}";"
+    
+     json parsedData = json::parse(jsonData);
+    
+    for (const auto& productData : parsedData["products"]) {
+        Product product;
+        product.name = productData["name"];
+        product.amount = productData["amount"];
+        product.price = productData["price"];
+        product.whoBoughtID = productData["whoBoughtID"];
+        products.push_back(product);
+    }
+
+    for (const auto& userData : parsedData["users"]) {
+        User user;
+        user.id = userData["id"];
+        user.name = userData["name"];
+        user.phone = userData["phone"];
+        users.push_back(user);
+    }
+  
+
+   
+    return 0;
+}
