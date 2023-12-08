5a4b6b06321cafaff3094ad901e8e445bfc0369e
wladek <135755358+wladek2136@users.noreply.github.com>
2023-12-08 23:21:46 +0100

    Update core.cpp

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -5,6 +5,7 @@
 
 using json = nlohmann::json;
 
+
 struct Product {
     std::string name;
     int amount;
@@ -17,34 +18,46 @@ struct User {
     std::string name;
     std::string phone;
 };
-int main() {
-    std::vector<Product> products;
-    std::vector<User> users;
-  
-  std::string jsonData = "
-  {"receipt":[{"products":[{"product":"sth","amount":sth,"price":sth,"whoBought",{"personiD":sth,
-"personID":sth}]},
-  {"product":"sth","amount":sth,"price":sth,"whoBought":2}]}],
-"users":[{id":1,"name":"sth","phone":"sth"},
-  {"id":2,"name":"sth","phone":"sth"}]}";"
-    
-     json parsedData = json::parse(jsonData);
-    
-    for (const auto& productData : parsedData["products"]) {
-        Product product;
-        product.name = productData["name"];
-        product.amount = productData["amount"];
-        product.price = productData["price"];
-        product.whoBoughtID = productData["whoBoughtID"];
-        products.push_back(product);
+
+void parseJsonData(const json& jsonData, std::vector<Product>& products, std::vector<User>& users) {
+    for (const auto& receiptData : jsonData["receipt"]) {
+        for (const auto& productData : receiptData["products"]) {
+            Product product;
+            product.name = productData["product"];
+            product.amount = productData["amount"];
+            product.price = productData["price"];
+
+            if (productData.contains("whoBought")) {
+                product.whoBoughtID = productData["whoBought"]["personID"];
+            } else {
+                product.whoBoughtID = productData["whoBought"];
+            }
+
+            products.push_back(product);
+        }
     }
 
-    for (const auto& userData : parsedData["users"]) {
+    for (const auto& userData : jsonData["users"]) {
         User user;
         user.id = userData["id"];
         user.name = userData["name"];
         user.phone = userData["phone"];
         users.push_back(user);
+    }
+}
+
+int main() {
+    std::vector<Product> products;
+    std::vector<User> users;
+    
+      {"receipt":[{"products":[{"product":"sth","amount":sth,"price":sth,"whoBought",{"personiD":sth,"personID":sth}]},
+      {"product":"sth","amount":sth,"price":sth,"whoBought":2}]}],
+       "users":[{id":1,"name":"sth","phone":"sth"},
+      {"id":2,"name":"sth","phone":"sth"}]}";
+    
+   json parsedData = json::parse(jsonData);
+
+    parseJsonData(parsedData, products, users);
     }
   
 
