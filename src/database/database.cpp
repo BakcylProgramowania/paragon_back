1c626c950059f5e057002d4921a8377324663a14
KuWojtow <kwojto07@gmail.com>
2023-12-10 15:07:09 +0100

    Creating lib for accessing database

--- src/database/database.cpp
+++ src/database/database.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
 
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_array;
+using bsoncxx::builder::basic::make_document;
+
+int main()
+{
+
+std::cout << "test";
+mongocxx::instance instance{};
+
+return 0;
+}
