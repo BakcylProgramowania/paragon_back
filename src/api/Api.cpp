189587bd26a5ea6bdfa5d85f2bd0836fbe00c421
ddnowicki <daniel.dominik.nowicki@gmail.com>
2023-12-13 13:46:36 +0100

    working get server

--- src/api/Api.cpp
+++ src/api/Api.cpp
@@ -1,2 +1,34 @@
+#include "./controller/MyController.hpp"
+#include "./AppComponent.hpp"
+
 #include "oatpp/network/Server.hpp"
-#include "oatpp/network/tcp/server/ConnectionProvider.hpp"
+
+#include <iostream>
+
+void run() {
+
+  /* Register Components in scope of run() method */
+  AppComponent components;
+
+  /* Get router component */
+  OATPP_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>, router);
+
+  /* Create MyController and add all of its endpoints to router */
+  router->addController(std::make_shared<MyController>());
+
+  /* Get connection handler component */
+  OATPP_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>, connectionHandler);
+
+  /* Get connection provider component */
+  OATPP_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, connectionProvider);
+
+  /* Create server which takes provided TCP connections and passes them to HTTP connection handler */
+  oatpp::network::Server server(connectionProvider, connectionHandler);
+
+  /* Print info about server port */
+  OATPP_LOGI("MyApp", "Server running on port %s", connectionProvider->getProperty("port").getData());
+
+  /* Run server */
+  server.run();
+  
+}
