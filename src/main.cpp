e41ef40bdc321f6775391aa4a06955f910165048
ddnowicki <daniel.dominik.nowicki@gmail.com>
2023-12-14 13:52:15 +0100

    add RAII

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,12 @@
+#include "api/EnvironmentWrapper.hpp"
 #include "api/Api.cpp"
 #include "example/Example.hpp"
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
+    EnvironmentWrapper environmentWrapper;
 
-  oatpp::base::Environment::init();
-
-  run();
-  
-  /* Print how much objects were created during app running, and what have left-probably leaked */
-  /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
-  std::cout << "\nEnvironment:\n";
-  std::cout << "objectsCount = " << oatpp::base::Environment::getObjectsCount() << "\n";
-  std::cout << "objectsCreated = " << oatpp::base::Environment::getObjectsCreated() << "\n\n";
-  
-  oatpp::base::Environment::destroy();
-  
-  return 0;
+    run();
+    
+    return 0;
 }
