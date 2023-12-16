094afdce90a27cea10a5f41ed45c7ed880beecdc
wladek <135755358+wladek2136@users.noreply.github.com>
2023-12-16 14:14:03 +0100

    Update core.cpp

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -92,20 +92,3 @@ void parseJsonData(const json& jsonData, std::vector<Product>& products, std::ve
     }
 }
 
-int main() 
-{
-    
-    std::vector<Product> products;
-    std::vector<User> users;
-    std::string jsonData;
-    
-   json parsedData = json::parse(jsonData);
-
-    parseJsonData(parsedData, products, users);
-    
-    }
-  
-
-   
-    return 0;
-}
