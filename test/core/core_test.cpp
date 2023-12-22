0882ba13a0c9ce85f40120d57c30a4c39c1c8ff8
wladek <135755358+wladek2136@users.noreply.github.com>
2023-12-22 19:50:38 +0100

    Update core_test.cpp

--- test/core/core_test.cpp
+++ test/core/core_test.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
-#include "core/core.cpp"
+#include "core/core.hpp"
 
-    const std::string jsonInput = R"(
+TEST(GetProductsTest, RetrievesProductsCorrectly) {
+
+    json jsonData = R"(
         {
             "receipt": [
                 {
@@ -24,58 +26,34 @@
                         }
                     ]
                 }
-            ],
+            ]
+        }
+    )"_json;
+
+    std::vector<Product> products = getProducts(jsonData);
+
+    ASSERT_EQ(products.size(), 2);
+}
+
+TEST(GetUsersTest, RetrievesUsersCorrectly) {
+    json jsonData = R"(
+        {
             "users": [
                 {
                     "id": 1,
-                    "name": "Wladek Mandziuk",
+                    "name": "John Doe",
                     "phone": "123-456-7890"
                 },
                 {
                     "id": 2,
-                    "name": "Michal Jakistam",
+                    "name": "Jane Doe",
                     "phone": "987-654-3210"
                 }
             ]
         }
-    )";
-
-TEST(ParseJsonDataTest, ParsesJsonDataCorrectly) 
-{
-
-    json jsonData = json::parse(jsonInput);
-
-    std::vector<Product> expectedProducts = {
-        {"Item1", 10.5, 2, 1},
-        {"Item2", 5.99, 1, 2}
-    };
-
-    std::vector<User> expectedUsers = {
-        {"Wladek Mandziuk", "123-456-7890", 1},
-        {"Michal Jakistam", "987-654-3210", 2}
-    };
-
-    std::vector<Product> actualProducts;
-    std::vector<User> actualUsers;
-
-      parseJsonData(jsonData, actualProducts, actualUsers);
-
-EXPECT_EQ(actualProducts.size(), expectedProducts.size());
-for (size_t i = 0; i < actualProducts.size(); ++i) 
-{
-    EXPECT_EQ(actualProducts[i].name, expectedProducts[i].name);
-    EXPECT_EQ(actualProducts[i].price, expectedProducts[i].price);
-    EXPECT_EQ(actualProducts[i].amount, expectedProducts[i].amount);
-    EXPECT_EQ(actualProducts[i].whoBoughtID, expectedProducts[i].whoBoughtID);
-}
-
-EXPECT_EQ(actualUsers.size(), expectedUsers.size());
-for (size_t i = 0; i < actualUsers.size(); ++i)
-{
-    EXPECT_EQ(actualUsers[i].name, expectedUsers[i].name);
-    EXPECT_EQ(actualUsers[i].phone, expectedUsers[i].phone);
-    EXPECT_EQ(actualUsers[i].id, expectedUsers[i].id);
-}
-
+    )"_json;
     
+    std::vector<User> users = getUsers(jsonData);
+
+    ASSERT_EQ(users.size(), 2);
 }
