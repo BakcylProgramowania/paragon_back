eed547fd33e42e41174feccc4f0d074a093b65aa
xDanielooo <73127307+ddnowicki@users.noreply.github.com>
2024-01-03 17:37:46 +0100

    add /login (#17)
    * add /login
    
    * formatting changed files
    
    ---------
    
    Co-authored-by: KuWojtow <kwojto07@gmail.com>

--- src/api/dto/DTOs.hpp
+++ src/api/dto/DTOs.hpp
@@ -10,10 +10,11 @@
  *  Used in API for serialization/deserialization and validation
  */
 class MyDto : public oatpp::DTO {
+ public:
   DTO_INIT(MyDto, DTO)
 
-  DTO_FIELD(Int32, statusCode);
-  DTO_FIELD(String, message);
+  DTO_FIELD(Int32, statusCode);  // Add this line
+  DTO_FIELD(String, message);    // Add this line
 };
 
 #include OATPP_CODEGEN_END(DTO)
