191eff86e3a81f5173ab19be5e1839ca969b0543
TStypulkowski <tymonstypulkowski2010@gmail.com>
2024-01-14 23:18:55 +0100

    fix intallation of mongodb

--- src/database/database.cpp
+++ src/database/database.cpp
@@ -1,12 +1,41 @@
 #include "database.hpp"
 
-Database::Database(const std::string& address)
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_array;
+using bsoncxx::builder::basic::make_document;
+
+class DatabaseImpl {
+  private:
+  mongocxx::uri uri;
+  mongocxx::client client;
+  mongocxx::database database;
+  bool isUserPasswordEqualGivenPassword(mongocxx::cursor& cursor,
+                                        const std::string& password) const;
+ public:
+  DatabaseImpl(const std::string& address);
+  ~DatabaseImpl() = default;
+
+  bool isThereUserWithThisUsername(const std::string& username) const;
+  bool isThereUserWithThisEmail(const std::string& email) const;
+  bool createUser(const std::string& username, const std::string& password,
+                  const std::string& email);
+  bool deleteUser(const std::string& id, const std::string& password);
+  bool loginCheck(const std::string& id, const std::string& password) const;
+};
+
+DatabaseImpl::DatabaseImpl(const std::string& address)
     : uri(mongocxx::uri(address)),
       client(mongocxx::client(uri)),
       database(client["ParagonApp"]) {}
 
 // Checks if is there user with given email
-bool Database::isThereUserWithThisEmail(const std::string& email) const {
+bool DatabaseImpl::isThereUserWithThisEmail(const std::string& email) const {
   auto collection = database["users"];
 
   // searching for user with this email and returning result
@@ -18,7 +47,7 @@ bool Database::isThereUserWithThisEmail(const std::string& email) const {
 }
 
 // Checks if is there user with given Username
-bool Database::isThereUserWithThisUsername(const std::string& username) const {
+bool DatabaseImpl::isThereUserWithThisUsername(const std::string& username) const {
   auto collection = database["users"];
 
   // searching for user with this username and returning result
@@ -29,7 +58,7 @@ bool Database::isThereUserWithThisUsername(const std::string& username) const {
     return false;
 }
 
-bool Database::isUserPasswordEqualGivenPassword(
+bool DatabaseImpl::isUserPasswordEqualGivenPassword(
     mongocxx::cursor& cursor, const std::string& password) const {
   for (auto&& doc : cursor) {
     // Geting user's Password and saving it as a userPassword
@@ -49,7 +78,7 @@ bool Database::isUserPasswordEqualGivenPassword(
 // returns true if user was created
 // returns false if there is already user with that username or email(new user
 // was not created)
-bool Database::createUser(const std::string& username,
+bool DatabaseImpl::createUser(const std::string& username,
                           const std::string& password,
                           const std::string& email) {
   auto collection = database["users"];
@@ -69,7 +98,7 @@ bool Database::createUser(const std::string& username,
 // needs password of this user
 // returns true if user was deleted and false if user was not deleted(password
 // is incorrect or there is no user with this id)
-bool Database::deleteUser(const std::string& id, const std::string& password) {
+bool DatabaseImpl::deleteUser(const std::string& id, const std::string& password) {
   auto collection = database["users"];
 
   // Checking if the 'id' is a Username or Email
@@ -99,7 +128,7 @@ bool Database::deleteUser(const std::string& id, const std::string& password) {
 
 // Checks if there is user with given 'id' and if user's password equals given
 // 'Password' returns true if 'id' and 'Password' are correct, false if not
-bool Database::loginCheck(const std::string& id,
+bool DatabaseImpl::loginCheck(const std::string& id,
                           const std::string& password) const {
   // id can be either email or username
   auto collection = database["users"];
@@ -118,3 +147,32 @@ bool Database::loginCheck(const std::string& id,
 
   return false;
 }
+
+  Database::Database(const std::string& address) : impl(new DatabaseImpl(address)) {};
+  Database::~Database() = default;
+  
+  bool Database::isThereUserWithThisUsername(const std::string& username) const
+  {
+    return impl->isThereUserWithThisUsername(username);
+  }
+
+  bool Database::isThereUserWithThisEmail(const std::string& email) const
+  {
+    return impl->isThereUserWithThisEmail(email);
+  }
+
+  bool Database::createUser(const std::string& username, const std::string& password,
+                  const std::string& email)
+  {
+    return impl->createUser(username, password, email);
+  }
+
+  bool Database::deleteUser(const std::string& id, const std::string& password)
+  {
+      return impl->deleteUser(id, password);
+  }
+
+  bool Database::loginCheck(const std::string& id, const std::string& password) const
+  {
+    return impl->loginCheck(id, password);
+  }
