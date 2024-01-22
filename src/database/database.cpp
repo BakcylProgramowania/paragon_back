8a1f95823c2a04412c006c68fae0ae856378323a
TStypulkowski <150160909+TStypulkowski@users.noreply.github.com>
2024-01-22 10:47:06 +0100

    Fix mongodb instalation (#39)
    * add bool function and test
    
    * add getLogin to core.hpp
    
    * add path to core in CMakeLists.txt
    
    * use getLogin func to communication with api
    
    * fix issue
    
    * some fix
    
    * add bool function and test
    
    * add getLogin to core.hpp
    
    * add path to core in CMakeLists.txt
    
    * use getLogin func to communication with api
    
    * some fix
    
    * fix issue
    
    * some fix
    
    * add connection core - db
    
    * uppdate core test
    
    * Update Dockerfile
    
    * fix some issues
    
    * fix issue
    
    * fix string names in db
    
    * update README.md
    
    * formated with clang
    
    * fix intallation of mongodb
    
    * add bool function and test
    
    * add path to core in CMakeLists.txt
    
    * some fix
    
    * fix issue
    
    * some fix
    
    * add path to core in CMakeLists.txt
    
    * some fix
    
    * fix issue
    
    * add connection core - db
    
    * fix some issues
    
    * formated with clang
    
    * fix intallation of mongodb
    
    * fix merge issues
    
    * formatted with clang
    
    * improve clarity of database code
    
    ---------
    
    Co-authored-by: ZMorosko <zuzanna.morosko@gmail.com>
    Co-authored-by: Jakub Drzewiecki <djakub712@gmail.com>
    Co-authored-by: Daniel <daniel.dominik.nowicki@gmail.com>
    Co-authored-by: xDanielooo <73127307+ddnowicki@users.noreply.github.com>

--- src/database/database.cpp
+++ src/database/database.cpp
@@ -1,120 +1,22 @@
 #include "database.hpp"
+#include "databaseImpl.hpp"
 
-Database::Database(const std::string& address)
-    : uri(mongocxx::uri(address)),
-      client(mongocxx::client(uri)),
-      database(client["ParagonApp"]) {}
 
-// Checks if is there user with given email
-bool Database::isThereUserWithThisEmail(const std::string& email) const {
-  auto collection = database["users"];
-
-  // searching for user with this email and returning result
-  auto cursor = collection.find_one(make_document(kvp("Email", email)));
-  if (cursor)
-    return true;
-  else
-    return false;
-}
-
-// Checks if is there user with given Username
-bool Database::isThereUserWithThisUsername(const std::string& username) const {
-  auto collection = database["users"];
-
-  // searching for user with this username and returning result
-  auto cursor = collection.find_one(make_document(kvp("UserName", username)));
-  if (cursor)
-    return true;
-  else
-    return false;
-}
-
-bool Database::isUserPasswordEqualGivenPassword(
-    mongocxx::cursor& cursor, const std::string& password) const {
-  for (auto&& doc : cursor) {
-    // Geting user's Password and saving it as a userPassword
-    auto userElementPassword = doc["Password"];
-    auto userPassword = userElementPassword.get_string().value;
-
-    // if 'userPassword' == 'Password' and return result
-    if (userPassword.compare(password) == 0) {
-      return true;
-    } else
-      return false;
-  }
-  return false;
-}
-
-// creating new user
-// returns true if user was created
-// returns false if there is already user with that username or email(new user
-// was not created)
-bool Database::createUser(const std::string& username,
-                          const std::string& password,
-                          const std::string& email) {
-  auto collection = database["users"];
-  if (isThereUserWithThisUsername(username) ||
-      isThereUserWithThisEmail(email)) {
-    return false;
-  } else {
-    auto resultOfInsert = collection.insert_one(
-        make_document(kvp("UserName", username), kvp("Password", password),
-                      kvp("Email", email)));
-  }
-  return true;
-}
-
-// deletes user
-// id can be either email or username
-// needs password of this user
-// returns true if user was deleted and false if user was not deleted(password
-// is incorrect or there is no user with this id)
-bool Database::deleteUser(const std::string& id, const std::string& password) {
-  auto collection = database["users"];
-
-  // Checking if the 'id' is a Username or Email
-  if (isThereUserWithThisUsername(id))  // id is an Username
+Database::Database(const std::string& address) : impl(new DatabaseImpl(address)) {};
+Database::~Database() = default;
+  
+  bool Database::createUser(const std::string& username, const std::string& password,
+                  const std::string& email)
   {
-    // Finding user by Username
-    auto cursor = collection.find(make_document(kvp("UserName", id)));
-    if (isUserPasswordEqualGivenPassword(cursor, password)) {
-      collection.delete_one(make_document(kvp("UserName", id)));
-      return true;
-    } else
-      return false;
-  } else if (isThereUserWithThisEmail(id))  // id is an Email
+    return impl->createUser(username, password, email);
+  }
+
+  bool Database::deleteUser(const std::string& id, const std::string& password)
   {
-    // Finding user by Email
-    auto cursor = collection.find(make_document(kvp("Email", id)));
-    if (isUserPasswordEqualGivenPassword(cursor, password)) {
-      collection.delete_one(make_document(kvp("Email", id)));
-      return true;
-    } else
-      return false;
-  } else
-    return false;  // there is no user with this id
+      return impl->deleteUser(id, password);
+  }
 
-  return false;
-}
-
-// Checks if there is user with given 'id' and if user's password equals given
-// 'Password' returns true if 'id' and 'Password' are correct, false if not
-bool Database::loginCheck(const std::string& id,
-                          const std::string& password) const {
-  // id can be either email or username
-  auto collection = database["users"];
-
-  // Checking if id is Username or Email
-  if (isThereUserWithThisUsername(id)) {
-    // Finding user by Username
-    auto cursor = collection.find(make_document(kvp("UserName", id)));
-    return isUserPasswordEqualGivenPassword(cursor, password);
-  } else if (isThereUserWithThisEmail(id)) {
-    // Finding user by Email
-    auto cursor = collection.find(make_document(kvp("Email", id)));
-    return isUserPasswordEqualGivenPassword(cursor, password);
-  } else
-    return false;
-
-  return false;
-}
+  bool Database::loginCheck(const std::string& id, const std::string& password) const
+  {
+    return impl->loginCheck(id, password);
+  }
