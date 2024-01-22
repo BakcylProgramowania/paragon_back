6c642e13a394452c807d6ea6965d3693ae79ab7c
TStypulkowski <150160909+TStypulkowski@users.noreply.github.com>
2024-01-22 10:47:06 +0100

    Fix mongodb instalation (#39)
    * add bool function and test
    
    * add getLogin to core.hpp
    
    * add path to core in CMakeLists.txt
    
    * use getLogin func to communication with api
    
    * fix issue
    
    * some fix
    
    * add bool function and test
    
    * add getLogin to core.hpp
    
    * add path to core in CMakeLists.txt
    
    * use getLogin func to communication with api
    
    * some fix
    
    * fix issue
    
    * some fix
    
    * add connection core - db
    
    * uppdate core test
    
    * Update Dockerfile
    
    * fix some issues
    
    * fix issue
    
    * fix string names in db
    
    * update README.md
    
    * formated with clang
    
    * fix intallation of mongodb
    
    * add bool function and test
    
    * add path to core in CMakeLists.txt
    
    * some fix
    
    * fix issue
    
    * some fix
    
    * add path to core in CMakeLists.txt
    
    * some fix
    
    * fix issue
    
    * add connection core - db
    
    * fix some issues
    
    * formated with clang
    
    * fix intallation of mongodb
    
    * fix merge issues
    
    * formatted with clang
    
    * improve clarity of database code
    
    ---------
    
    Co-authored-by: ZMorosko <zuzanna.morosko@gmail.com>
    Co-authored-by: Jakub Drzewiecki <djakub712@gmail.com>
    Co-authored-by: Daniel <daniel.dominik.nowicki@gmail.com>
    Co-authored-by: xDanielooo <73127307+ddnowicki@users.noreply.github.com>

--- src/database/database.hpp
+++ src/database/database.hpp
@@ -1,35 +1,20 @@
 #pragma once
 
-#include <bsoncxx/builder/basic/document.hpp>
-#include <bsoncxx/json.hpp>
-#include <cstdint>
-#include <iostream>
-#include <mongocxx/client.hpp>
-#include <mongocxx/instance.hpp>
-#include <mongocxx/stdx.hpp>
-#include <mongocxx/uri.hpp>
-#include <vector>
+#include <string>
+#include <memory>
 
-using bsoncxx::builder::basic::kvp;
-using bsoncxx::builder::basic::make_array;
-using bsoncxx::builder::basic::make_document;
+class DatabaseImpl;
 
 class Database {
- private:
-  mongocxx::uri uri;
-  mongocxx::client client;
-  mongocxx::database database;
-
  public:
   Database(const std::string& address);
-  ~Database() = default;
+  ~Database();
 
-  bool isThereUserWithThisUsername(const std::string& username) const;
-  bool isThereUserWithThisEmail(const std::string& email) const;
-  bool isUserPasswordEqualGivenPassword(mongocxx::cursor& cursor,
-                                        const std::string& password) const;
   bool createUser(const std::string& username, const std::string& password,
                   const std::string& email);
   bool deleteUser(const std::string& id, const std::string& password);
   bool loginCheck(const std::string& id, const std::string& password) const;
+
+  private:
+  std::unique_ptr<DatabaseImpl> impl;
 };
