1702f77c951f3b2aa1d45817d8762a6a88b74567
Daniel <daniel.dominik.nowicki@gmail.com>
2024-02-06 11:30:48 +0100

    semicolon added

--- test/core/core_test.cpp
+++ test/core/core_test.cpp
@@ -83,7 +83,7 @@ TEST(GetUsersTest, RetrievesUsersCorrectly) {
 
 TEST(WhetherUserIsCorrect, ReturnTrueForUserJan34) {
   Database database("mongodb+srv://"
-          "bakcyl324:Bakcyl768324@paragondatabase.jedczob.mongodb.net/")
+          "bakcyl324:Bakcyl768324@paragondatabase.jedczob.mongodb.net/");
   Authenticator auth(database);
   EXPECT_FALSE(auth.authenticateUser("Jan34", "Jan905").empty());
 }
