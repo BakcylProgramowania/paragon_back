0242dfa24bb13bba04c043f8f295047a8ccaff9d
xDanielooo <daniel.dominik.nowicki@gmail.com>
2024-02-14 18:07:08 +0100

    Update appbuild.sh

--- appbuild.sh
+++ appbuild.sh
@@ -6,10 +6,7 @@ fi
 
 git clone -b $BRANCH $REPO paragon_back
 cd paragon_back
-mkdir build
-cd build
-cmake -GNinja ..
-cd ..
+cmake -Bbuild -GNinja
 cmake --build build --target all
 ctest --test-dir build
 cd ..
