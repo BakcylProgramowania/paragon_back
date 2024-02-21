9802d8726b73c6f9656aa29d49c5600c23da0a0c
PStepniak19 <ps2007@o2.pl>
2024-02-21 16:07:30 +0100

    Adding CalculateOperationOnReceipt function

---
+++ src/core/CalculateOperationOnReceipt.cpp
@@ -0,0 +1,22 @@
+#include "CalculateOperationOnReceipt.hpp"
+
+std::vector<User>& CalculateOperationOnReceipt::calculateReceipt(const std::vector<Item>& items) const{
+    std::vector<User> users;
+
+    for(auto item : items){
+        int cost = item.price * item.amount;
+        bool foundEqualUserID = false;
+        for(auto user : users){
+                if(user.userID == item.whoBuy){
+                    user.price += cost;
+                    foundEqualUserID = true;
+                    break;
+                }
+        } 
+        if (!foundEqualUserID) {
+            User user = {item.whoBuy, cost};
+            users.push_back(user);
+        }
+    }
+    return users;
+};
---
+++ src/core/CalculateOperationOnReceipt.hpp
@@ -0,0 +1,7 @@
+#include <vector>
+#include "structures.hpp"
+
+class CalculateOperationOnReceipt{
+    public:
+    std::vector<User>& calculateReceipt(const std::vector<Item>& items) const;
+};
