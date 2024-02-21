7e5e71012f835a9e601e25aca44871ce154293d3
PStepniak19 <ps2007@o2.pl>
2024-02-21 16:01:32 +0100

    Adding structures

---
+++ src/core/structures.hpp
@@ -0,0 +1,13 @@
+#include <string>
+
+struct User{
+    std::string userID;
+    int price;
+};
+
+struct Item{
+    std::string whoBuy;
+    std::string itemName;
+    int price;
+    int amount;
+};
