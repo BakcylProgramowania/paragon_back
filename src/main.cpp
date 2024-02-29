f90478083b19f97acb6557bc5da6ba58353cdf4b
Daniel <daniel.dominik.nowicki@gmail.com>
2024-02-29 13:28:27 +0100

    delete core.cpp & hpp pt2

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,6 @@
 
 #include "api/Api.cpp"
 #include "api/EnvironmentWrapper.hpp"
-#include "core/core.cpp"
-#include "database/database.hpp"
-#include "example/Example.hpp"
 
 int main(int argc, const char* argv[]) {
   EnvironmentWrapper environmentWrapper;
