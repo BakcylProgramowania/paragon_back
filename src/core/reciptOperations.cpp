b990fae3981c85d89078e1e1525213aa166df87b
Daniel <daniel.dominik.nowicki@gmail.com>
2024-02-29 20:37:14 +0100

    fix #66

--- src/core/reciptOperations.cpp
+++ src/core/reciptOperations.cpp
@@ -6,25 +6,27 @@ std::vector<User> ReciptOperations::calculateReceipt(
     const std::vector<Item>& items) const {
   std::vector<User> users;
 
-  for (auto item : items) {
+  for (const auto& item : items) {
     int cost = item.price * item.amount;
     bool foundEqualUserID = false;
-    for (auto user : users) {
+
+    for (auto& user : users) {
       if (user.userID == item.whoBuy) {
         user.price += cost;
         foundEqualUserID = true;
         break;
       }
     }
+
     if (!foundEqualUserID) {
       User user = {item.whoBuy, cost};
       users.push_back(user);
     }
   }
 
-  for (auto user : users) {
-    database.changeBalance(user.userID,
-                           database.getBalance(user.userID) + user.price);
+  for (const auto& user : users) {
+    database.changeBalance(user.userID, database.getBalance(user.userID) + user.price);
   }
+
   return users;
-};
+}
