1e12ef8b3534655b2957e71cd3313866786223ac
TStypulkowski <tymonstypulkowski2010@gmail.com>
2024-03-21 21:28:47 +0100

    add getUserIDUsingUsername function, from now on functions addUserToFriendList, removeUserFromFriendList use username instead of userId

--- src/database/database.cpp
+++ src/database/database.cpp
@@ -47,13 +47,13 @@ std::vector<std::pair<std::string, std::string>> bakcyl::database::Database::ret
 }
 
 bool bakcyl::database::Database::addUserToFriendList(const std::string& token,
-                                   const std::string& friendIdToAdd) const {
-  return bakcyl::database::Database::impl->addUserToFriendList(token, friendIdToAdd);
+                                   const std::string& friendUsernameToAdd) const {
+  return bakcyl::database::Database::impl->addUserToFriendList(token, friendUsernameToAdd);
 }
 
 bool bakcyl::database::Database::removeUserFromFriendList(
-    const std::string& token, const std::string& friendIdToRemove) const {
-  return bakcyl::database::Database::impl->removeUserFromFriendList(token, friendIdToRemove);
+    const std::string& token, const std::string& friendUsernameToRemove) const {
+  return bakcyl::database::Database::impl->removeUserFromFriendList(token, friendUsernameToRemove);
 }
 
 std::string bakcyl::database::Database::getUserIDUsingToken(const std::string& token) const {
@@ -78,6 +78,10 @@ bool bakcyl::database::Database::changeIfMerged(const std::string& receiptID, co
 
 std::vector<bakcyl::core::ReceiptShortView> bakcyl::database::Database::getReceipts(const std::string& authorID) {
   return bakcyl::database::Database::impl->getReceipts(authorID);
+}
+
+std::string bakcyl::database::Database::getUserIDUsingUsername(const std::string& username) const {
+  return bakcyl::database::Database::impl->getUserIDUsingUsername(username);
 }
 
 }
