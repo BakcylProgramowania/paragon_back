671e695e55e962e1267f8ec7bd32a94eb43e2fda
Daniel <daniel.dominik.nowicki@gmail.com>
2024-03-28 19:18:58 +0100

    change utest username

--- test/database/databaseTest.cpp
+++ test/database/databaseTest.cpp
@@ -10,19 +10,16 @@ TEST(DatabaseLogingTest, ExpectedValidLoginToWork) {
       "mongodb+srv://"
       "bakcyl324:Bakcyl768324@paragondatabase.jedczob.mongodb.net/");
   EXPECT_FALSE(database.loginCheck("Lucy", "wrongPassword"));
-  EXPECT_FALSE(database.loginCheck("nonExistingUser", "ycuL"));
-  EXPECT_FALSE(database.loginCheck("nonExistingEmail@wrong.wrong", "ycuL"));
-  EXPECT_TRUE(database.loginCheck("Lucy", "ycuL"));
-  EXPECT_TRUE(database.loginCheck("lucy00@test.com", "ycuL"));
-  EXPECT_TRUE(database.isThereUserWithThisID("65786066b8bbf529cbc56fc1"));
+  EXPECT_TRUE(database.loginCheck("utest", "uTest123"));
+  EXPECT_TRUE(database.isThereUserWithThisID("6605b3d13431efc35a0d7cb7"));
 }
 
 TEST(DatabaseBalanceTest, ExpectedValidBalanceToWork) {
   bakcyl::database::Database database(
       "mongodb+srv://"
       "bakcyl324:Bakcyl768324@paragondatabase.jedczob.mongodb.net/");
-  EXPECT_TRUE(database.changeBalance("65b3c6e05cf3bd901c04aa52", 0.00));
-  EXPECT_EQ(database.getBalance("65b3c6e05cf3bd901c04aa52"), 0.00);
-  EXPECT_TRUE(database.changeBalance("65b3c6e05cf3bd901c04aa52", 15.55));
-  EXPECT_EQ(database.getBalance("65b3c6e05cf3bd901c04aa52"), 15.55);
+  EXPECT_TRUE(database.changeBalance("6605b3d13431efc35a0d7cb7", 10.00));
+  EXPECT_EQ(database.getBalance("6605b3d13431efc35a0d7cb7"), 10.00);
+  EXPECT_TRUE(database.changeBalance("6605b3d13431efc35a0d7cb7", 15.55));
+  EXPECT_EQ(database.getBalance("6605b3d13431efc35a0d7cb7"), 15.55);
 }
